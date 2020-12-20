d4130b8c7430ebc07b04d15e5099d53e678586ac
Herny <ht020@hdm-stuttgart.de>
2020-12-20 14:33:15 +0100

    ProjectType.py

--- E-ProjectManagement/frontend/src/api/index.js
+++ E-ProjectManagement/frontend/src/api/index.js
@@ -1,10 +1,10 @@
 
 export {default as ManagementAPi} from './ManagementAPI';
-export {default as TransactionBO} from './TransactionBO';
 export {default as ModuleBO} from './ModuleBO';
 export {default as ParticipationBO} from './ParticipationBO';
 export {default as PersonBO} from './PersonBO';
 export {default as ProjectBO } from './ProjectBO';
+export {default as ProjectTypeBO} from './ProjectType'
 export {default as RatingBO} from './RatingBO';
 export {default as RoleBO} from './RoleBO';
 export {default as SemesterBO} from './SemesterBO';
