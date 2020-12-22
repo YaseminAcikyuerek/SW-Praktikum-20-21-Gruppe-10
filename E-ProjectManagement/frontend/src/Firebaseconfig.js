e69782fb9d85dac7762a5f93446ce6e53912ece8
Herny <ht020@hdm-stuttgart.de>
2020-12-22 13:00:02 +0100

    ProjectType.py

---
+++ E-ProjectManagement/frontend/src/Firebaseconfig.js
@@ -0,0 +1,13 @@
+/** The firebase config structure for the Python Bank Project as provided by the firebase admin website */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZK2K6U4mGKCN9o7f98NFodnIw7dsnF0s",
+  authDomain: "bankprojekt-f37b6.firebaseapp.com",
+  databaseURL: "https://bankprojekt-f37b6.firebaseio.com",
+  projectId: "bankprojekt-f37b6",
+  storageBucket: "bankprojekt-f37b6.appspot.com",
+  messagingSenderId: "512657507610",
+  appId: "1:512657507610:web:88b57bdd25489e47d018a3"
+};
+
+export default firebaseConfig;
