5b9772d76e6af62c04fde9a9212ac7f25758d592
Herny <ht020@hdm-stuttgart.de>
2020-12-24 01:36:55 +0100

    ProjectType.py

--- E-ProjectManagement/frontend/src/api/ProjectTypeBO.js
+++ E-ProjectManagement/frontend/src/api/ProjectTypeBO.js
@@ -1,6 +1,45 @@
 import BusinessObject from './BusinessObject';
 
 /**
- * Represents a customer of the bank.
+ * R
  */
 export default class ProjectTypeBO extends BusinessObject {
+
+constructor(aSws,aEcts) {
+    super();
+    this.sws = aSws;
+    this.ects = aEcts;
+  }
+
+  setSws(aSws) {
+  this.sws = aSws;
+  }
+
+  getSws() {
+    return this.sws
+  }
+  setEcts(aEcts) {
+  this.ects = aEcts;
+  }
+
+  getEcts() {
+    return this.Ects
+  }
+
+  static fromJSON(ProjectType) {
+    let result = [];
+
+    if (Array.isArray(project_types)) {
+      project_types.forEach((c) => {
+        Object.setPrototypeOf(c, ProjectTypeBO.prototype);
+        result.push(c);
+      })
+    } else {
+      //
+      let c = project_types;
+      Object.setPrototypeOf(c, ProjectTypeBO.prototype);
+      result.push(c);
+    }
+
+    return result;
+  }
