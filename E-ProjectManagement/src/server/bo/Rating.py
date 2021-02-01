4356a8a6d04fa3fb4f4bb7b6bb7850765227404e
EnesT98 <et018@hdm-stuttgart.de>
2021-02-01 20:53:28 +0100

    Rating.py korrigiert

--- E-ProjectManagement/src/server/bo/Rating.py
+++ E-ProjectManagement/src/server/bo/Rating.py
@@ -16,28 +16,28 @@ class Rating(bo):
     def get_project(self):
         return self._project
 
-    def set_evaluator(self,person):
-        self._evaluator= person
+    def set_evaluator(self, person):
+        self._evaluator = person
 
     def get_evaluator(self):
         return self._evaluator
 
-    def set_to_be_assessed(self,student):
-        self._to_be_assessed= student
+    def set_to_be_assessed(self, student):
+        self._to_be_assessed = student
 
     def get_to_be_assessed(self):
         return self._to_be_assessed
 
-    def set_grade(self,value):
+    def set_grade(self, value):
         self._grade = value
 
     def get_grade(self):
         return self._grade
 
-    def set_passed(self,value):
+    def set_passed(self, value):
         self._passed = value
 
-    def get_passed (self):
+    def get_passed(self):
         return self._passed
 
 
